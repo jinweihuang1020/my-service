8b15db5786aba17a0b11aa330457cb82d64343da
jinweihuang <jinweihuang@gpmcorp.com.tw>
2020-12-22 15:58:03 +0800

    page test

---
+++ WebApplication1/Controllers/PorkController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PorkController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            var ret = await GetPorkPriceToday();
+            return new string[] { ret };
+        }
+
+        public  async Task<string> GetPorkPriceToday()
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var response = await client.GetAsync("https://agridata.coa.gov.tw/api/v1/PorkTransType/?MarketName=%E5%98%89%E7%BE%A9%E7%B8%A3");
+                    bool isResponse = response.IsSuccessStatusCode;
+                    if (isResponse)
+                    {
+                        return await response.Content.ReadAsStringAsync();
+                    }
+                    else
+                        return "Fail";
+                }
+                catch (Exception ex)
+                {
+                    //EventLog(ex.Message);
+                    Console.WriteLine(ex.Message);
+                    return ex.Message;
+                }
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpPost("Login")]
+        public IActionResult Login([FromBody] UserInfo user_info)
+        {
+            if (user_info.Name == "123")
+                return BadRequest("forbid");
+            return Ok();
+        }
+
+    }
+}
