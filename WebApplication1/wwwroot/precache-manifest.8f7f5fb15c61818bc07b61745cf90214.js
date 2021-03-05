a9b86c548d090c88be368b620972f7bcaefbd272
jinweihuang <jinweihuang@gpmcorp.com.tw>
2021-03-05 10:28:14 +0800

    update

---
+++ WebApplication1/wwwroot/precache-manifest.8f7f5fb15c61818bc07b61745cf90214.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe398d99e1dd6f0d759c",
+    "url": "/css/app.4d1f97cb.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0ea44ba2a194d2ad5925f06a51aad2be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dc140f0f79f6142801a",
+    "url": "/js/about.53c0e1e4.js"
+  },
+  {
+    "revision": "fe398d99e1dd6f0d759c",
+    "url": "/js/app.87c9242f.js"
+  },
+  {
+    "revision": "515881354836bec1358e",
+    "url": "/js/chunk-vendors.3775b62a.js"
+  },
+  {
+    "revision": "096f5949a95593e1fa7e4a2cb243d057",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
