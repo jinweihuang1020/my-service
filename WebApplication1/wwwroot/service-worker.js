acc4a3f8019cf90357dc09c04f6420417b40e064
jinweihuang <jinweihuang@gpmcorp.com.tw>
2021-03-05 14:05:21 +0800

    Update webpage

--- WebApplication1/wwwroot/service-worker.js
+++ WebApplication1/wwwroot/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.daa577d8c3655bee2e8d818e4c28ed1a.js"
+  "/precache-manifest.abd1fa54f11df97d1b4da09b952c4518.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "gin_wei_reume"});
