7e0742b982b099f2ba701ddf125df7b7c4bf2522
jinweihuang <jinweihuang@gpmcorp.com.tw>
2021-03-17 09:53:28 +0800

    test

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -20,7 +20,7 @@ namespace WebApplication1
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-               .UseUrls("http://localhost:5001");
+                .UseStartup<Startup>();
+                //.UseUrls("http://localhost:5001");
     }
 }
